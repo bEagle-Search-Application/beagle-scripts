5884ab6acd5ae3f3e62a13ce0e781d337275a3be
danimc97 <dani1997medina@gmail.com>
2022-07-13 00:10:14 +0200

    Add frontend path

--- beagleStartProject.sh
+++ beagleStartProject.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-userId=$(id -u)
 option=$1
 beagleFrontDirectory=~/projects/beagle-front-project/
 beagleBackDirectory=~/projects/beagle-project/
 
-cd $beagleBackDirectory && ./docker/$1
-
+cd $beagleBackDirectory && ./docker/$option
+cd $beagleFrontDirectory && ./docker/$option
